25d75b83153a4a254b6f04eb3404ae41bb01fd65
George <george.hodgkins@colorado.edu>
2021-12-06 09:23:30 -0700

    Add dependencies, C++ize header

--- ledgrid.h
+++ ledgrid.h
@@ -1,9 +1,17 @@
 #ifndef _LEDGRID_H_
 #define _LEDGRID_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int grid_init (void);
 int grid_select (unsigned);
 int grid_free (void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #define LEDGRID_ROWS 4
 #define LEDGRID_COLS 4
